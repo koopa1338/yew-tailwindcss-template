7484b15149d1e7fdf67f1a5c0ca70a983f74f026
koopa1338 <sinner1991@gmail.com>
2022-08-13 21:04:14 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![recursion_limit = "1024"]
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use console_error_panic_hook::set_once as set_panic_hook;
+use yew::prelude::*;
+
+struct App;
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <h1>{"Hello World"}</h1>
+        }
+    }
+}
+
+fn main() {
+    set_panic_hook();
+
+    yew::start_app::<App>();
+}
